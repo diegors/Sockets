32674fb59b07e521e75474bf548e69488693f262
droig <diego.roig@metlife.com>
2017-07-14 16:48:08 +0100

    Adding 2 projects

---
+++ ClientSocket/src/client/com/Requester.java
@@ -0,0 +1,95 @@
+package client.com;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Requester {
+	Socket requestSocket;
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	String message;
+	Scanner inText;
+
+	Requester() {
+	}
+
+	void run(String param[]) {
+		try {
+			// out.close();
+			// 1. creating a socket to connect to the server
+			requestSocket = new Socket(param[0], Integer.parseInt(param[1]));
+			System.out.println("Connected Server to" + param[0] + " in port " + param[1]);
+			// 2. get Input and Output streams
+			out = new ObjectOutputStream(requestSocket.getOutputStream());
+			out.flush();
+			in = new ObjectInputStream(requestSocket.getInputStream());
+			// 3: Communicating with the server
+			try {
+				message = (String) in.readObject();
+
+				do {
+
+					System.out.println("server>" + message);
+					// prompt for the Enter Project Folder Address
+					System.out.print("Enter message to send to Server: ");
+
+					// create a scanner so we can read the command-line input
+					inText = new Scanner(System.in);
+					// get their input as a String
+					String sendMessage = inText.nextLine();
+					sendMessage(sendMessage);
+					if (sendMessage.equals("bye")) {
+						message = (String) in.readObject();
+
+					}
+
+					//
+
+					/*
+					 * sendMessage("Hi my server"); message = "bye";
+					 * sendMessage(message);
+					 */
+
+				} while (!message.equals("bye"));
+			} catch (ClassNotFoundException classNot) {
+				System.err.println("data received in unknown format");
+			}
+
+		} catch (UnknownHostException unknownHost) {
+			System.err.println("You are trying to connect to an unknown host!");
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} finally {
+			// 4: Closing connection
+			try {
+
+				inText.close();
+
+				in.close();
+				out.close();
+				requestSocket.close();
+				
+				System.out.println("****Disconnection****");
+				
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+	}
+
+	void sendMessage(String msg) {
+		try {
+			out.writeObject(msg);
+			out.flush();
+			System.out.println("client>" + msg);
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+
+	public static void main(String args[]) {
+		Requester client = new Requester();
+		client.run(args);
+	}
+}
---
+++ ServerSocket/src/server/com/Provider.java
@@ -0,0 +1,80 @@
+package server.com;
+
+import java.io.*;
+import java.net.*;
+
+public class Provider {
+	ServerSocket providerSocket;
+	Socket connection = null;
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	String message;
+
+	Provider() {
+	}
+
+	void run(String param[]) {
+		try {
+			// in.close();
+			// 1. creating a server socket
+			providerSocket = new ServerSocket(Integer.parseInt(param[0]), 10);
+			// 2. Wait for connection
+			System.out.println("Waiting for connection");
+			connection = providerSocket.accept();
+			System.out.println(
+					"Connection received from " + connection.getInetAddress().getHostName());
+			// 3. get Input and Output streams
+			out = new ObjectOutputStream(connection.getOutputStream());
+			out.flush();
+			in = new ObjectInputStream(connection.getInputStream());
+			sendMessage("Connection successful");
+			// 4. The two parts communicate via the input and output streams
+			do {
+				try {
+					message = (String) in.readObject();
+					System.out.println("client>" + message);
+					if (message.equals("bye"))
+						sendMessage("bye");
+				} catch (ClassNotFoundException classnot) {
+					System.err.println("Data received in unknown format");
+				}
+			} while (!message.equals("bye"));
+			
+			// 4: Closing connection
+			in.close();
+			out.close();
+			providerSocket.close();
+
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} finally {
+			// 4: Closing connection
+			
+			try {
+				in.close();
+				out.close();
+				providerSocket.close();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+			
+		}
+	}
+
+	void sendMessage(String msg) {
+		try {
+			out.writeObject(msg);
+			out.flush();
+			System.out.println("server>" + msg);
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+
+	public static void main(String args[]) {
+		Provider server = new Provider();
+		while (true) {
+			server.run(args);
+		}
+	}
+}
